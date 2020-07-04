a65c61b2bfac3221f4f40a73682bdbab2f011344
JCP5 <justen.chong@gmail.com>
2020-07-04 01:47:56 -0500

    BeatDetection scene

---
+++ QuickJam0619/Assets/Justen/Code/ShaderBeatEmission.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class ShaderBeatEmission : MonoBehaviour
+{
+    public float bpm;
+    public float bps;
+    public float emissiveIntesity = 1;
+
+    public float secondsPerBeat = 1;
+    public float fadeOutStrength = 1;
+
+    public TilemapRenderer tmr;
+    public Material mat;
+
+    public float beatOffset;
+    public float offsetTime;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        bps = bpm / 60f;
+        secondsPerBeat = 1 / bps;
+        offsetTime = beatOffset * secondsPerBeat;
+
+        tmr = this.GetComponent<TilemapRenderer>();
+        mat = tmr.material;
+        mat.SetFloat("_Frequency", 0);
+    }
+
+    private void Update()
+    {
+        if (beatOffset > 0)
+        {
+            if (offsetTime > 0)
+            {
+                offsetTime -= Time.deltaTime;
+            }
+            else
+            {
+                FadeLight();
+            }
+        }
+        else
+        {
+            FadeLight();
+        }
+    }
+
+    void FadeLight()
+    {
+        if (secondsPerBeat > 0)
+        {
+            secondsPerBeat -= Time.deltaTime;
+
+            float intensity = mat.GetFloat("_Frequency") - Time.deltaTime * (emissiveIntesity) * fadeOutStrength;
+            mat.SetFloat("_Frequency", intensity);
+        }
+        else
+        {
+            secondsPerBeat = 1 / bps;
+            mat.SetFloat("_Frequency", emissiveIntesity);
+            offsetTime = beatOffset * secondsPerBeat;
+        }
+    }
+}
