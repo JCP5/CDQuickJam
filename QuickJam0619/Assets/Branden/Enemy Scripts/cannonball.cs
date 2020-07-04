1ff6a46873a8990a19174a1c88666bb49e24cf8d
Branden Kunst <brandenkunst@gmail.com>
2020-07-04 01:28:58 -0500

    Enemies/gunsfx/

---
+++ QuickJam0619/Assets/Branden/Enemy Scripts/cannonball.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cannonball : MonoBehaviour
+{
+    public GameObject cannonHitParticles;
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+
+        Debug.Log("??");
+        if(collision.gameObject.CompareTag("Player") || (collision.gameObject.CompareTag("Walls")))
+        {
+            Instantiate(cannonHitParticles, transform.position, Quaternion.identity);
+            Destroy(this.gameObject);
+        }
+    }
+}
